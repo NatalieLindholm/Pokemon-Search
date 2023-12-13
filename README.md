c1902e10522f194ea2a7f488aea51add3a950e4b
Natalie Lindholm <natalie.lindholm05@gmail.com>
2023-12-13 13:53:33 +0200

    13.12

--- README.md
+++ README.md
@@ -3,10 +3,6 @@ Skapa en Pokémon-söksida med hjälp av React och Tailwind CSS. Använda Vite s
 
 Det är viktigt att du designar och “stylar” webbappen så professionellt och fint som möjligt.
 
-Funktioner
-Sökruta och Sökknapp:
-Implementera en användarvänlig sökruta och en tillhörande sökknapp på din webbsida.
-Sökrutan bör tillåta användaren att skriva in Pokémon-namn.
 Användning av Pokémon API:
 Integrera fetch-funktionen för att hämta data från Pokémon API:en (https://pokeapi.co).
 Använd användarens inmatning från sökrutan för att skapa en dynamisk förfrågan till API:en och hämta relevant Pokémon-information.
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-// import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './index.css'
 
 function App() {
+const [data, setData] = useState();
 
+  useEffect(() => {
+    const api = async () => {
+      const pokemonApi = await fetch("https://pokeapi.co/api/v2/pokemon/ditto", {
+        method: 'GET'
+      })
+      const pokemonData = await pokemonApi.json()
+    console.log(pokemonData);
+    
+    }
+
+    api()
+  }, [])
 
   return (
     <>
       <div className="bg-blue-100 h-screen">
-<h1>lol</h1>
+<h1>Pokemon Search</h1>
+<input type="text" placeholder='Search pokemon...' />
+<button>Search</button>
+
+<div>
+  {/* display search here */}
+<h2>name</h2>
+<img src="" alt="" />
+<p>Height:</p>
+<p>Weight:</p>
+<p>Type:</p>
+<p>Stats:</p>
+<p>Abilities:</p>
+<p>Moves:</p>
+</div>
+
       </div>
     </>
   )
